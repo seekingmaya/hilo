bff1a4e540270de3edce79af67b785b6d81ceded
Vitalii <denberry81@gmail.com>
2019-01-06 14:34:06 +0200

    add media query to js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,18 @@
 import "../scss/main.scss";
 import '../img/favicon.ico';
-
 require('intersection-observer');
+import {
+    generateSlides,
+    shuffledData
+}
 
-import { generateSlides, shuffledData } from './generateSlides';
-import { debug } from "util";
+    from './generateSlides';
+import {
+    debug
+}
 
+    from "util";
 generateSlides();
-
 window.addEventListener('load', function () {
     let sliderLogo = document.querySelectorAll(".logo__wrapper");
     let container = document.querySelector(".container");
@@ -25,30 +30,25 @@ window.addEventListener('load', function () {
     let backgroundFirst = true;
     let artists = document.querySelectorAll(".artist__item");
     let navScrollInProgress = false;
+    let desktop = window.matchMedia("(min-width: 1366px)");
+    let portraitOrientation = window.matchMedia("(orientation: portrait)");
+    let landscapeOrientation = window.matchMedia("(orientation: landscape)");
+    let landscape = false;
 
-
-    //show info
-
-    sliderLogo.forEach(logo => {
-        logo.addEventListener('click', (e) => {
-            // e.stopPropagation();
-            container.classList.toggle("show-info");
-        });
-    });
 
     //intro animation
-    if (document.documentElement.clientWidth > 1366) {
+    if (desktop.matches) {
         let currentSlide = document.querySelector(".show-slide");
-
         currentSlide.addEventListener("animationend", () => {
             // setTimeout(changeBorder, 850, 0);
             currentSlide.style.animation = "initial";
             document.documentElement.style.setProperty('--edge-width', "5px");
             changeBorder(0);
             runTimer();
-        });
+        }
+        );
     }
-    else if (document.documentElement.clientWidth < document.documentElement.clientHeight) {
+    else if (portraitOrientation.matches) {
         let currentSlide = document.querySelector(".show-slide");
         currentSlide.style.animation = "initial";
         runTimer();
@@ -56,10 +56,34 @@ window.addEventListener('load', function () {
         window.addEventListener('touchstart', detectInteraction);
         slider.addEventListener('wheel', detectInteraction);
     }
-    else if (document.documentElement.clientWidth > document.documentElement.clientHeight) {
+    else {
         let currentSlide = document.querySelector(".show-slide");
         currentSlide.style.animation = "initial";
     }
+
+    portraitOrientation.addListener(portraitHandler);
+    landscapeOrientation.addListener(landscapeHandler);
+
+    function portraitHandler(mq) {
+        if (landscape) {
+            window.addEventListener('click', detectInteraction);
+            window.addEventListener('touchstart', detectInteraction);
+            slider.addEventListener('wheel', detectInteraction);
+            landscape = false;
+        }
+        // else {
+
+        // }
+
+    }
+
+
+    function landscapeHandler(mq) {
+        landscape = true;
+        clearInterval(timerId);
+    }
+
+
 
     function detectInteraction() {
         clearInterval(timerId);
@@ -70,81 +94,75 @@ window.addEventListener('load', function () {
     }
 
 
+    //show info
+    sliderLogo.forEach(logo => {
+        logo.addEventListener('click', (e) => {
+            // e.stopPropagation();
+            container.classList.toggle("show-info");
+        }
+        );
+    }
+    );
+
+
 
     //scroll to slide when nav clicked
-
     scrollNav.forEach(el => {
         el.addEventListener('click', e => {
             if (TweenLite) {
-                e.preventDefault();
-                // e.stopPropagation();
+                e.preventDefault(); // e.stopPropagation();
                 let n = parseInt(el.href.split("#slide")[1]);
                 let topOffset = slider.clientHeight * n;
-
                 navScrollInProgress = true;
-                TweenLite.to(slider, 1, { scrollTo: topOffset, onComplete: onComplete });
-
-
+                TweenLite.to(slider, 1, {
+                    scrollTo: topOffset, onComplete: onComplete
+                }
+                );
                 scrollNav.forEach(nav => nav.classList.remove("scrolled"));
-
                 el.classList.add("scrolled");
             }
-        });
-    });
-
-    //prevents nav dots for slides that's not the tagret lighting up when user clicks on nav
-
+        }
+        );
+    }
+    ); //prevents nav dots for slides that's not the tagret lighting up when user clicks on nav
     function onComplete() {
         navScrollInProgress = false;
     }
-
     const intersectionObserver = new IntersectionObserver((entries, observer) => {
         entries.forEach((entry) => {
-
-
             let artist = artists[parseInt(entry.target.dataset.id)];
-
             if (entry.intersectionRatio >= 0.6) {
-
                 artists.forEach(artist => artist.style.zIndex = "-1");
-
                 artist.style.zIndex = "1";
-
                 slideIndex = parseInt(entry.target.dataset.id);
                 let newSlide = slides[slideIndex];
-
                 if (!newSlide.classList.contains("show-slide")) {
-
                     let currentSlide = document.querySelector(".show-slide");
                     currentSlide.classList.remove("show-slide");
                     newSlide.classList.add("show-slide");
-                }
-
-
-                //show active nav when corresponding slide is visible
-
+                } //show active nav when corresponding slide is visible
                 if (!navScrollInProgress) {
                     scrollNav.forEach(nav => {
                         nav.classList.remove("scrolled");
                         if (entry.target.id == nav.href.split("#")[1]) {
                             nav.classList.add("scrolled");
                         }
-                    });
+                    }
+                    );
                 }
-
-
+            } //change opacity for artist name when its slide is in viewport
+            artist.style.opacity = `$ {
+                entry.intersectionRatio >=0.6 ? 1: 0
             }
-
-            //change opacity for artist name when its slide is in viewport
-
-            artist.style.opacity = `${entry.intersectionRatio >= 0.6 ? 1 : 0}`;
-
-        });
-    }, { threshold: 0.6 });
-
+            `;
+        }
+        );
+    }
+        , {
+            threshold: 0.6
+        }
+    );
     slides.forEach((element) => intersectionObserver.observe(element));
-
-
     function moveLeft(e) {
         let current = document.querySelector(".show-slide");
         let currentNumber = parseInt(current.dataset.id);
@@ -152,22 +170,17 @@ window.addEventListener('load', function () {
             // if (e) e.stopPropagation();
             let newIndex = currentNumber > 0 ? currentNumber - 1 : slides.length - 1;
             let newSlide = slides[newIndex];
-
             current.classList.remove("show-slide");
-
             if (TweenLite) {
-
-                TweenLite.to(slider, 0, { scrollTo: slider.clientHeight * newIndex });
+                TweenLite.to(slider, 0, {
+                    scrollTo: slider.clientHeight * newIndex
+                }
+                );
             }
-
             newSlide.classList.add("show-slide");
             setTimeout(changeBorder, 850, newIndex);
-
         }
-    }
-
-    //show slide to the right
-
+    } //show slide to the right
     function moveRight(e) {
         let current = document.querySelector(".show-slide");
         let currentNumber = parseInt(current.dataset.id);
@@ -175,25 +188,22 @@ window.addEventListener('load', function () {
             // if (e) e.stopPropagation();
             let newIndex = currentNumber < slides.length - 1 ? currentNumber + 1 : 0;
             let newSlide = slides[newIndex];
-
             current.classList.remove("show-slide");
-
             if (TweenLite) {
-
                 setTimeout(() => {
-                    TweenLite.to(slider, 0, { scrollTo: slider.clientHeight * newIndex });
-                }, 850);
+                    TweenLite.to(slider, 0, {
+                        scrollTo: slider.clientHeight * newIndex
+                    }
+                    );
+                }
+                    , 850);
             }
-
             newSlide.classList.add("show-slide");
             setTimeout(changeBorder, 850, newIndex);
         }
-    }
-
-    //change cubes edge background when gallary image changes
-
+    } //change cubes edge background when gallary image changes
     function changeBorder(newIndex) {
-        let slide = shuffledData[newIndex]
+        let slide = shuffledData[newIndex];
         let url = `url('../img/${slide.firstname} ${slide.lastname}.png')`;
         if (!backgroundFirst) {
             background1.style.backgroundImage = url;
@@ -207,20 +217,14 @@ window.addEventListener('load', function () {
             background1.style.opacity = '0';
             background2.style.opacity = '1';
         }
-
         backgroundFirst = !backgroundFirst;
-
-    }
-
-    //change slides every 5secs
-
+    } //change slides every 5secs
     function runTimer() {
         timerId = setInterval(() => {
             moveRight();
-        }, 3000);
+        }
+            , 3000);
     }
-
-
     function checkTimer() {
         if (cube.classList.contains("d__cube--big")) {
             clearInterval(timerId);
@@ -229,13 +233,8 @@ window.addEventListener('load', function () {
             clearInterval(timerId);
             runTimer();
         }
-
-    }
-
-    //zoom in 
-
+    } //zoom in 
     sliderWrapper.addEventListener('click', zoom);
-
     function zoom(e) {
         sliderParent.classList.toggle("slider--big");
         cube.classList.toggle("d__cube--big");
@@ -243,47 +242,36 @@ window.addEventListener('load', function () {
         if (!cube.classList.contains("d__cube--big")) {
             changeBorder(parseInt(document.querySelector(".show-slide").dataset.id));
         }
-    }
-
-
-    //change slides on key press
-
+    } //change slides on key press
     window.addEventListener('keydown', (e) => {
         if (e.defaultPrevented) {
             return; // Do nothing if the event was already processed
         }
-
         switch (e.key) {
-
             case "Left": // IE/Edge specific value
                 moveLeft(e);
                 checkTimer();
-            case "ArrowLeft":
-                moveLeft(e);
+            case "ArrowLeft": moveLeft(e);
                 checkTimer();
                 break;
             case "Right": // IE/Edge specific value
                 moveRight(e);
                 checkTimer();
-            case "ArrowRight":
-                moveRight(e);
+            case "ArrowRight": moveRight(e);
                 checkTimer();
                 break;
-            case " ":
-                zoom(e);
+            case " ": zoom(e);
                 break;
             case "Esc": // IE/Edge specific value
                 zoom(e);
-            case "Escape":
-                // Do something for "esc" key press.
+            case "Escape": // Do something for "esc" key press.
                 zoom(e);
                 break;
-            default:
-                return; // Quit when this doesn't handle the key event.
-        }
-
-        // Cancel the default action to avoid it being handled twice
+            default: return; // Quit when this doesn't handle the key event.
+        } // Cancel the default action to avoid it being handled twice
         event.preventDefault();
-    }, true);
+    }
+        , true);
+}
 
-});
+);
