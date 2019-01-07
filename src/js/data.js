88d4f9917fdd40f00eef22c1fca291c9b9203e62
Vitalii <denberry81@gmail.com>
2019-01-07 18:07:12 +0200

    add  scale and antializing to prevent blutty text

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,7 @@
 let data = [
 
     { firstname: 'camila', lastname: 'lamarca', backgroundPosition: "center", desktopSize: '60%' },
-    { firstname: 'carolina', lastname: 'zancolli', backgroundPosition: "50% 30%", desktopSize: '60%' },
+    { firstname: 'carolina', lastname: 'zancolli', backgroundPosition: "50% 30%", desktopSize: '50%' },
     { firstname: 'claudia', lastname: 'cortinez', backgroundPosition: "center", desktopSize: '50%' },
     { firstname: 'florencia', lastname: 'walter', backgroundPosition: "center", desktopSize: '60%' },
     { firstname: 'samuel', lastname: 'lasso', backgroundPosition: "60% 70%", desktopSize: '60%' },
