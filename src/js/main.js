65168ef7faddcbec69c1af8d262645c5f281d780
Vitalii <denberry81@gmail.com>
2019-01-15 22:13:21 +0200

    change css transition to js

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import "../scss/main.scss";
 import '../img/favicon.ico';
 require('intersection-observer');
 import { generateSlides, shuffledData } from './generateSlides';
+import BezierEasing from './BezierEasing';
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -18,11 +19,14 @@ window.addEventListener('load', function () {
     let slider = document.querySelector(".slider__content");
     let sliderParent = document.querySelector(".slider");
     let slides = document.querySelectorAll(".slide");
+    let prevSlideIndex = 0;
     let cube = document.querySelector('.d__cube');
     let back = document.querySelector(".d__cube-face--back");
     let sliderWrapper = document.querySelector(".slider__wrapper");
     let timerId;
     let slideIndex = 0;
+    let artistTimeout;
+    let animationID;
     let background1 = document.querySelector(".container__background--1");
     let background2 = document.querySelector(".container__background--2");
     let blackCube = document.querySelector(".container__background--3");
@@ -31,8 +35,10 @@ window.addEventListener('load', function () {
     let backgroundFirst = true;
     let artists = document.querySelectorAll(".artist__item");
     let navScrollInProgress = false;
+    let zoomTransform = "matrix(2.08, 0, 0, 2.08, 0, 0)";
     let tablet = window.matchMedia(`(max-width: 1024px),(min-device-width: 1024px) and (max-device-width: 1024px)
     ,(device-width: 1366px) and (device-height: 1024px) `);
+
 
     function init() {
         if (!tablet.matches) {
@@ -110,50 +116,111 @@ window.addEventListener('load', function () {
                 //prevents nav dots for slides that's not the tagret lighting up when user clicks on nav
                 TweenLite.to(slider, 1, { scrollTo: topOffset, onComplete: () => navScrollInProgress = false });
                 activateNav(el);
+
+                changeBorderifZoomedOut(n);
+
+                showArtistName(n);
+
+                // prevSlideIndex = n;
             }
         }
         );
-    }
-    );
+    });
 
     function activateNav(nav) {
         scrollNav.forEach(nav => nav.classList.remove("scrolled"));
         nav.classList.add("scrolled");
     }
 
+    function showArtistName(index) {
+        let currentArtist = artists[prevSlideIndex];
+
+        currentArtist.style.zIndex = "-1";
+
+        animate({ draw(progress) { currentArtist.style.opacity = 1 - progress; } });
+
+        artists.forEach(el => {
+            el.style.opacity = "0";
+            el.style.zIndex = "-1";
+        });
+
+
+        let newArtist = artists[index];
+        newArtist.style.zIndex = "1";
+
+        clearTimeout(artistTimeout);
+
+        artistTimeout = setTimeout(() => { animate({ draw(progress) { newArtist.style.opacity = progress; } }); }, 850);
+    }
+
     const intersectionObserver = new IntersectionObserver((entries, observer) => {
         entries.forEach((entry) => {
-            let artist = artists[parseInt(entry.target.dataset.id)];
+
+            let currentSlide = document.querySelector(".show-slide");
+
             if (entry.intersectionRatio >= 0.6) {
                 if (!navScrollInProgress) { }
-                artists.forEach(artist => {
-                    artist.style.zIndex = "-1";
-                    artist.style.opacity = "0";
-                });
-                artist.style.zIndex = "1";
-                artist.style.opacity = "1";
 
-                slideIndex = parseInt(entry.target.dataset.id);
+                let slideIndex = parseInt(entry.target.dataset.id);
                 let newSlide = slides[slideIndex];
 
+
                 if (!newSlide.classList.contains("show-slide")) {
-                    let currentSlide = document.querySelector(".show-slide");
+
+
                     currentSlide.classList.remove("show-slide");
                     newSlide.classList.add("show-slide");
-                    setTimeout(changeBorder, 850, slideIndex);
+
+                    changeBorderifZoomedOut(slideIndex)
+
                 }
                 //show active nav when corresponding slide is visible
                 if (!navScrollInProgress) {
                     let nav = document.querySelector(`[href='#slide${slideIndex}']`);
                     activateNav(nav);
 
+                    showArtistName(slideIndex);
+
                 }
+
+                prevSlideIndex = slideIndex;
             }
         }
         );
     }, { threshold: 0.6 });
 
     slides.forEach((element) => intersectionObserver.observe(element));
+
+    //animate transition
+
+    function animate(options) {
+
+        let start = performance.now();
+
+        let duration = 850;
+
+        let calcBezier = BezierEasing(.455, .03, .515, .955);
+
+        function timing(timeFraction) {
+            return calcBezier(timeFraction);
+        }
+
+        requestAnimationFrame(function animate(time) {
+            // timeFraction от 0 до 1
+            let timeFraction = (time - start) / duration;
+            if (timeFraction > 1) timeFraction = 1;
+
+            // текущее состояние анимации
+            let progress = timing(timeFraction)
+
+            options.draw(progress);
+
+            if (timeFraction < 1) {
+                requestAnimationFrame(animate);
+            }
+
+        });
+    }
 
 
     function moveUp(e) {
@@ -164,13 +231,23 @@ window.addEventListener('load', function () {
             let newIndex = currentNumber > 0 ? currentNumber - 1 : slides.length - 1;
             let newSlide = slides[newIndex];
             current.classList.remove("show-slide");
+
+            animate({ draw(progress) { current.style.opacity = 1 - progress; } });
             if (TweenLite) {
-                TweenLite.to(slider, 0, { scrollTo: slider.clientHeight * newIndex });
+                setTimeout(() => {
+                    TweenLite.to(slider, 0, { scrollTo: slider.clientHeight * newIndex });
+
+                    animate({ draw(progress) { newSlide.style.opacity = progress; } });
+
+                }, 850);
             }
             newSlide.classList.add("show-slide");
-            setTimeout(changeBorder, 850, newIndex);
+
+            changeBorderifZoomedOut(newIndex);
         }
-    } //show slide to the right
+    }
+
+    //show slide lower
     function moveDown(e) {
         let current = document.querySelector(".show-slide");
         let currentNumber = parseInt(current.dataset.id);
@@ -178,19 +255,34 @@ window.addEventListener('load', function () {
             let newIndex = currentNumber < slides.length - 1 ? currentNumber + 1 : 0;
             let newSlide = slides[newIndex];
             current.classList.remove("show-slide");
+
+            animate({ draw(progress) { current.style.opacity = 1 - progress; } });
+
             if (TweenLite) {
                 setTimeout(() => {
-                    TweenLite.to(slider, 0, {
-                        scrollTo: slider.clientHeight * newIndex
-                    }
-                    );
-                }
-                    , 850);
+                    TweenLite.to(slider, 0, { scrollTo: slider.clientHeight * newIndex });
+
+                    animate({ draw(progress) { newSlide.style.opacity = progress; } });
+
+                }, 850);
             }
+
             newSlide.classList.add("show-slide");
-            setTimeout(changeBorder, 850, newIndex);
+
+            changeBorderifZoomedOut(newIndex);
+
         }
-    } //change cubes edge background when gallary image changes
+    }
+
+
+    //change cubes edge background when gallary image changes
+
+    function changeBorderifZoomedOut(index) {
+        if (getComputedStyle(cube).getPropertyValue('transform') != zoomTransform) {
+            setTimeout(changeBorder, 850, index);
+        }
+    }
+
     function changeBorder(newIndex) {
         let slide = shuffledData[newIndex];
         let url = `url('../img/${slide.firstname} ${slide.lastname}.png')`;
@@ -207,17 +299,30 @@ window.addEventListener('load', function () {
             background2.style.opacity = '1';
         }
         backgroundFirst = !backgroundFirst;
-    } //change slides every 4secs
+    }
+
+
+    //change slides every 4secs
     function runTimer() {
         timerId = setInterval(() => { moveDown() }, 4000);
     }
     function stopTimer() {
         clearInterval(timerId);
-    } //zoom in 
+    }
+
+    //zoom in 
     sliderWrapper.addEventListener('click', zoom);
+
     function zoom() {
         sliderParent.classList.toggle("slider--big");
         cube.classList.toggle("d__cube--big");
+
+        if (cube.style.transform != zoomTransform) {
+            let current = document.querySelector(".show-slide");
+            let currentNumber = parseInt(current.dataset.id);
+            changeBorder(currentNumber);
+
+        }
     }
     //change slides on key press
     window.addEventListener('keydown', (e) => {
