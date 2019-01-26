67e25f3058c6e2dbf3acaaa00c27fe47e847f51e
Vitalii <denberry81@gmail.com>
2019-01-26 17:12:59 +0200

    remove console logs

--- src/js/positionSlider.js
+++ src/js/positionSlider.js
@@ -6,9 +6,8 @@ export function positionSlider(slider) {
     mediaQueryList.addListener(preserveSliderPosition);
 
     slider.addEventListener('scroll', () => {
-        console.log(`in scroll handler - prevOffset beg ${prevOffset} scrollTop ${slider.scrollTop} slider.scrollHeight ${slider.scrollHeight}`)
         prevOffset = Number.parseFloat(slider.scrollTop / slider.scrollHeight).toFixed(2)
-        console.log(`in scroll handler - prevOffset end ${prevOffset} scrollTop ${slider.scrollTop} slider.scrollHeight ${slider.scrollHeight}`)
+
     })
 
 
@@ -17,11 +16,8 @@ export function positionSlider(slider) {
         let copyPrevOffset = prevOffset;
         setTimeout(() => {
             let offset = (slider.scrollHeight * parseFloat(copyPrevOffset));
-            console.log(`before tweenlite - copyPrevOffset ${copyPrevOffset} offset ${offset} prevOffset ${prevOffset} scrollTop ${slider.scrollTop} slider.scrollHeight ${slider.scrollHeight}`)
             TweenLite.to(slider, 0, { scrollTo: offset, onComplete: () => { console.log(`in onComplete handler for TweenLite - prevOffset ${prevOffset} scrollTop ${slider.scrollTop} slider.scrollHeight ${slider.scrollHeight}`) } });
             slider.style.opacity = "1";
-            // prevOffset = Number.parseFloat(slider.scrollTop / slider.scrollHeight).toFixed(2);
-            console.log(`after tweenlite - offset ${offset} prevOffset ${prevOffset} scrollTop ${slider.scrollTop} slider.scrollHeight ${slider.scrollHeight}`)
         }, 0);
 
 
